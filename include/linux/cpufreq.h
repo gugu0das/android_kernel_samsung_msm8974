2a04ed54f70cbb7cf3a65abd6c0268b5b3c5de10
yank555-lu <yank555.lu@gmail.com>
2014-04-26 12:26:37 +0200

    drivers: cpufreq: Add Yankactive Governor
    Interactive governor with, fow now :
    
    - hispeed_freq defaulting to lower than policy_max (~1.5GHz)
    - target_loads increased to 95
    - min_sampling time reduced to 20ms
    - timer_rate increased to 60ms
    - above_hispeed_delay increased to 80ms
    
    Conflicts:
    
    	drivers/cpufreq/Kconfig
    	drivers/cpufreq/Makefile

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -488,6 +488,9 @@ extern struct cpufreq_governor cpufreq_gov_intellimm;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_ALUCARD)
 extern struct cpufreq_governor cpufreq_gov_alucard;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_alucard)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_YANKACTIVE)
+extern struct cpufreq_governor cpufreq_gov_yankactive;
+#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_yankactive)
 #endif
 
 
