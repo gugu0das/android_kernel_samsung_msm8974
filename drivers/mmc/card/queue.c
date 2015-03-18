84f8fad89ae99661be298f2ee8d38f15db03dfeb
Zhao Wei Liew <zhaoweiliew@gmail.com>
2015-03-18 13:14:43 +0800

    mmc : card : Schedule mmcqd on idle CPU
    Schedule the mmc queue thread on an idle CPU
    to boost mmc performance.
    
    Signed-off-by: Zhao Wei Liew <zhaoweiliew@gmail.com>

--- drivers/mmc/card/queue.c
+++ drivers/mmc/card/queue.c
@@ -69,6 +69,7 @@ static int mmc_queue_thread(void *d)
         sched_setscheduler(current, SCHED_FIFO, &scheduler_params);
 
 	current->flags |= PF_MEMALLOC;
+	set_wake_up_idle(true);
 
 	down(&mq->thread_sem);
 	do {
