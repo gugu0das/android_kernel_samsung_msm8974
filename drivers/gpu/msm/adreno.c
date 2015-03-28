463cdfa9523e056affd75fe03d787171d5faeae2
googyanas <googyanas@gmail.com>
2015-03-28 23:27:19 +0000

    kgsl : Modify which MMU clocks are enabled/disabled
    There is no need to try to attach a clock if it is already attached,
    or detach a clock if it is already detached. Restructure this
    logic to only attach/detach the clocks when needed. As well as
    protect ourselves using the MMU lock more readily.
    
    Signed-off-by: Carter Cooper <ccooper@codeaurora.org>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- drivers/gpu/msm/adreno.c
+++ drivers/gpu/msm/adreno.c
@@ -2161,8 +2161,6 @@ static int adreno_stop(struct kgsl_device *device)
 	adreno_dispatcher_stop(adreno_dev);
 	adreno_ringbuffer_stop(&adreno_dev->ringbuffer);
 
-	kgsl_mmu_stop(&device->mmu);
-
 	device->ftbl->irqctrl(device, 0);
 	kgsl_pwrctrl_irq(device, KGSL_PWRFLAGS_OFF);
 	del_timer_sync(&device->idle_timer);
@@ -2171,6 +2169,8 @@ static int adreno_stop(struct kgsl_device *device)
 
 	/* Save physical performance counter values before GPU power down*/
 	adreno_perfcounter_save(adreno_dev);
+
+	kgsl_mmu_stop(&device->mmu);
 
 	/* Power down the device */
 	kgsl_pwrctrl_disable(device);
@@ -2918,6 +2918,8 @@ int adreno_soft_reset(struct kgsl_device *device)
 		return -EINVAL;
 	}
 
+	clear_bit(ADRENO_DEVICE_STARTED, &adreno_dev->priv);
+
 	if (adreno_dev->drawctxt_active)
 		kgsl_context_put(&adreno_dev->drawctxt_active->base);
 
@@ -2968,7 +2970,10 @@ int adreno_soft_reset(struct kgsl_device *device)
 	if (ret)
 		return ret;
 
-	device->reset_counter++;
+	else {
+		device->reset_counter++;
+		set_bit(ADRENO_DEVICE_STARTED, &adreno_dev->priv);
+	}
 
 	return 0;
 }
