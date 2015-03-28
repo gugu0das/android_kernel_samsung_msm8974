cdf7122854f1ce8d654a3e86cb25bbdf61f38d41
googyanas <googyanas@gmail.com>
2015-03-28 23:13:58 +0000

    kgsl : Check for mmu pagefault before recovery
    Check whether there is a pagefault before running recovery.
    If recovery runs before the bottom pagefault handler runs
    then there could be a pending pagefault at end of recovery that
    can stall the IOMMU. With the IOMMU stalled the GPU would only
    read back zeroes even after recovery.
    
    Signed-off-by: Shubhraprakash Das <sadas@codeaurora.org>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- drivers/gpu/msm/adreno_dispatch.c
+++ drivers/gpu/msm/adreno_dispatch.c
@@ -1068,6 +1068,9 @@ static int dispatcher_do_fault(struct kgsl_device *device)
 		adreno_writereg(adreno_dev, ADRENO_REG_CP_ME_CNTL, reg);
 	}
 
+	/* Set pagefault if it occurred */
+	kgsl_mmu_set_pagefault(&device->mmu);
+
 	adreno_readreg(adreno_dev, ADRENO_REG_CP_IB1_BASE, &base);
 
 	/*
