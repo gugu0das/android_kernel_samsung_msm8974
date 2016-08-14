fcd0a96bb55bb1482a273eaad62882e0f4eaa417
gugu0das <gugu0das@gmail.com>
2016-08-14 23:40:10 +0900

    Upstream : Add 3.4.10 Missing Code

--- drivers/usb/host/xhci.h
+++ drivers/usb/host/xhci.h
@@ -1488,6 +1488,8 @@ struct xhci_hcd {
 #define XHCI_RESET_ON_RESUME	(1 << 7)
 #define	XHCI_SW_BW_CHECKING	(1 << 8)
 #define XHCI_AMD_0x96_HOST	(1 << 9)
+#define XHCI_TRUST_TX_LENGTH	(1 << 10)
+#define XHCI_SPURIOUS_REBOOT	(1 << 13)
 /*
  * In Synopsis DWC3 controller, PORTSC register access involves multiple clock
  * domains. When the software does a PORTSC write, handshakes are needed
@@ -1511,7 +1513,6 @@ struct xhci_hcd {
  * The workaround is to give worst case pipe delay ~350us after resetting HC
  */
 #define XHCI_RESET_DELAY	(1 << 11)
-#define XHCI_TRUST_TX_LENGTH	(1 << 12)
 	unsigned int		num_active_eps;
 	unsigned int		limit_active_eps;
 	/* There are two roothubs to keep track of bus suspend info for */
