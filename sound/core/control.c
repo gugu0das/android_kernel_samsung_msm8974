c1eeca6e9dd68b28739c9ea27c706438e554c5ec
gugu0das <gugu0das@gmail.com>
2016-12-15 23:24:49 +0900

    Upstream : 3.4.95

--- sound/core/control.c
+++ sound/core/control.c
@@ -287,10 +287,11 @@ static bool snd_ctl_remove_numid_conflict(struct snd_card *card,
 					  unsigned int count)
 {
 	struct snd_kcontrol *kctl;
+
 	/* Make sure that the ids assigned to the control do not wrap around */
 	if (card->last_numid >= UINT_MAX - count)
 		card->last_numid = 0;
-	
+
 	list_for_each_entry(kctl, &card->controls, list) {
 		if (kctl->id.numid < card->last_numid + 1 + count &&
 		    kctl->id.numid + kctl->count > card->last_numid + 1) {
@@ -332,7 +333,7 @@ int snd_ctl_add(struct snd_card *card, struct snd_kcontrol *kcontrol)
 {
 	struct snd_ctl_elem_id id;
 	unsigned int idx;
-	unsigned int count;	
+	unsigned int count;
 	int err = -EINVAL;
 
 	if (! kcontrol)
@@ -342,7 +343,7 @@ int snd_ctl_add(struct snd_card *card, struct snd_kcontrol *kcontrol)
 	id = kcontrol->id;
 	if (id.index > UINT_MAX - kcontrol->count)
 		goto error;
-	
+
 	down_write(&card->controls_rwsem);
 	if (snd_ctl_find_id(card, &id)) {
 		up_write(&card->controls_rwsem);
@@ -364,7 +365,7 @@ int snd_ctl_add(struct snd_card *card, struct snd_kcontrol *kcontrol)
 	card->controls_count += kcontrol->count;
 	kcontrol->id.numid = card->last_numid + 1;
 	card->last_numid += kcontrol->count;
-	count = kcontrol->count;	
+	count = kcontrol->count;
 	up_write(&card->controls_rwsem);
 	for (idx = 0; idx < count; idx++, id.index++, id.numid++)
 		snd_ctl_notify(card, SNDRV_CTL_EVENT_MASK_ADD, &id);
@@ -904,9 +905,9 @@ static int snd_ctl_elem_write(struct snd_card *card, struct snd_ctl_file *file,
 			result = kctl->put(kctl, control);
 		}
 		if (result > 0) {
-			struct snd_ctl_elem_id id = control->id;			
+			struct snd_ctl_elem_id id = control->id;
 			up_read(&card->controls_rwsem);
-			snd_ctl_notify(card, SNDRV_CTL_EVENT_MASK_VALUE, &id);			
+			snd_ctl_notify(card, SNDRV_CTL_EVENT_MASK_VALUE, &id);
 			return 0;
 		}
 	}
@@ -1340,7 +1341,7 @@ static int snd_ctl_tlv_ioctl(struct snd_ctl_file *file,
 		}
 		err = kctl->tlv.c(kctl, op_flag, tlv.length, _tlv->tlv);
 		if (err > 0) {
-			struct snd_ctl_elem_id id = kctl->id;			
+			struct snd_ctl_elem_id id = kctl->id;
 			up_read(&card->controls_rwsem);
 			snd_ctl_notify(card, SNDRV_CTL_EVENT_MASK_TLV, &id);
 			return 0;
