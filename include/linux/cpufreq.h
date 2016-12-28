ac816142db2f1ce516b73e5191cd3ce7055d7c94
gugu0das <gugu0das@gmail.com>
2016-12-29 01:46:07 +0900

    cpufreq : Add arteractive Governor

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -467,6 +467,9 @@ extern struct cpufreq_governor cpufreq_gov_intellidemand;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_INTELLIACTIVE)
 extern struct cpufreq_governor cpufreq_gov_intelliactive;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_intelliactive)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_ARTERACTIVE)
+extern struct cpufreq_governor cpufreq_gov_arteractive;
+#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_arteractive)
 #endif
 
 
