7066d7a5f09b72a8b42949f28554b4cc15898dfa
gugu0das <gugu0das@gmail.com>
2017-02-23 22:53:26 +0900

    Support gugu0das Kernel For MIUI8

--- build_kernel-ks01lte.sh
+++ build_kernel-ks01lte.sh
@@ -5,11 +5,12 @@ export CROSS_COMPILE=$(pwd)/arm-eabi-ubertc_7.0-2016.04/bin/arm-eabi-
 
 mkdir output
 
-# Touchwiz ks01lteskt Kernel
+# Touchwiz / MIUI8 ks01lteskt Kernel
 make -C $(pwd) O=output msm8974_sec_defconfig VARIANT_DEFCONFIG=msm8974_sec_ks01_skt_defconfig SELINUX_DEFCONFIG=selinux_defconfig
 make -j64 -C $(pwd) O=output
 
 cp output/arch/arm/boot/zImage $(pwd)/gugu0das/Touchwiz/kernel_ks01lteskt
+cp output/arch/arm/boot/zImage $(pwd)/gugu0das/MIUI8/kernel_ks01lteskt
 
 # Clean
 ./cleanup.sh
@@ -17,11 +18,12 @@ cp output/arch/arm/boot/zImage $(pwd)/gugu0das/Touchwiz/kernel_ks01lteskt
 # Create Output Directory
 mkdir output
 
-# Touchwiz ks01lteeur Kernel
+# Touchwiz / MIUI8 ks01lteeur Kernel
 make -C $(pwd) O=output msm8974_sec_defconfig VARIANT_DEFCONFIG=msm8974_sec_ks01eur_defconfig SELINUX_DEFCONFIG=selinux_defconfig
 make -j64 -C $(pwd) O=output
 
 cp output/arch/arm/boot/zImage $(pwd)/gugu0das/Touchwiz/kernel_ks01lteeur
+cp output/arch/arm/boot/zImage $(pwd)/gugu0das/MIUI8/kernel_ks01lteeur
 
 # Make Boot.img
 ./gugu0das/mkboot.sh
--- cleanup.sh
+++ cleanup.sh
@@ -6,7 +6,14 @@ rm -r -f output/
 rm -f gugu0das/Touchwiz/ramdisk.cpio.gz
 rm -f gugu0das/Touchwiz/boot_ks01lteskt.img
 rm -f gugu0das/Touchwiz/boot_ks01lteeur.img
+rm -f gugu0das/MIUI8/ramdisk.cpio.gz
+rm -f gugu0das/MIUI8/boot_ks01lteskt.img
+rm -f gugu0das/MIUI8/boot_ks01lteeur.img
 rm -f gugu0das/gugu0das_kernel-NX_TW-M-ks01lteskt-Release-1/boot.img
 rm -f gugu0das/gugu0das_kernel-NX_TW-M-ks01lteeur-Release-1/boot.img
+rm -f gugu0das/gugu0das_kernel-NX_MIUI8-M-ks01lteskt-Release-1/boot.img
+rm -f gugu0das/gugu0das_kernel-NX_MIUI8-M-ks01lteeur-Release-1/boot.img
 rm -f gugu0das_kernel-NX_TW-M-ks01lteskt-Release-1.zip
 rm -f gugu0das_kernel-NX_TW-M-ks01lteeur-Release-1.zip
+rm -f gugu0das_kernel-NX_MIUI8-M-ks01lteskt-Release-1.zip
+rm -f gugu0das_kernel-NX_MIUI8-M-ks01lteeur-Release-1.zip
