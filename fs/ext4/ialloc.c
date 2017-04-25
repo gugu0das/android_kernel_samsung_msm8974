6401c0c1c7fedde3718e42363a303b2302f55929
gugu0das <gugu0das@gmail.com>
2017-04-25 11:06:16 +0900

    fs : ext4 : ialloc : Remove print_bh() in ext4_free_inode()

--- fs/ext4/ialloc.c
+++ fs/ext4/ialloc.c
@@ -297,7 +297,6 @@ out:
 			fatal = err;
 		ext4_mark_super_dirty(sb);
 	} else {
-		print_bh(sb, bitmap_bh, 0, EXT4_BLOCK_SIZE(sb));
 		ext4_error(sb, "bit already cleared for inode %lu", ino);
 	}
 
