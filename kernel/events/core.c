9973c7c165b8a3bc2a46c489225e94c40bf6f04c
gugu0das <gugu0das@gmail.com>
2017-04-28 11:20:52 +0900

    kernel : events : core : Update perf_remove_from_context()

--- kernel/events/core.c
+++ kernel/events/core.c
@@ -1265,7 +1265,7 @@ static void perf_retry_remove(struct perf_event *event)
  * When called from perf_event_exit_task, it's OK because the
  * context has been detached from its task.
  */
-static void __ref perf_remove_from_context(struct perf_event *event, bool detach_group)
+static void perf_remove_from_context(struct perf_event *event, bool detach_group)
 {
 	struct perf_event_context *ctx = event->ctx;
 	struct task_struct *task = ctx->task;
@@ -1273,18 +1273,15 @@ static void __ref perf_remove_from_context(struct perf_event *event, bool detach
 		.event = event,
 		.detach_group = detach_group,
 	};
-	int ret;
 
 	lockdep_assert_held(&ctx->mutex);
 
 	if (!task) {
 		/*
-		 * Per cpu events are removed via an smp call
+		 * Per cpu events are removed via an smp call and
+		 * the removal is always successful.
 		 */
-		ret = cpu_function_call(event->cpu, __perf_remove_from_context,
-					&re);
-		if (ret == -ENXIO)
-			perf_retry_remove(event);
+		cpu_function_call(event->cpu, __perf_remove_from_context, &re);
 		return;
 	}
 
