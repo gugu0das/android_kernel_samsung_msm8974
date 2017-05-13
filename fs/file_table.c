8ee7b26c11c713abe7d55b13f8cd851b2a5c3f84
gugu0das <gugu0das@gmail.com>
2017-05-13 14:30:45 +0900

    fs : file_table : Add DEFINE_LGLOCK(files_lglock)

--- fs/file_table.c
+++ fs/file_table.c
@@ -35,6 +35,7 @@ struct files_stat_struct files_stat = {
 };
 
 DECLARE_LGLOCK(files_lglock);
+DEFINE_LGLOCK(files_lglock);
 
 /* SLAB cache for file structures */
 static struct kmem_cache *filp_cachep __read_mostly;
