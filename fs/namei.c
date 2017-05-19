ec8a203ac54547abc3868229f4ba46dd2a9a590b
gugu0das <gugu0das@gmail.com>
2017-05-20 01:04:30 +0900

    fs : namei : Revert 'Add sdcardfs Header File'
    This reverts commit b3ab054f72d2cd6e5246d874dc6205a1fa63a971.

--- fs/namei.c
+++ fs/namei.c
@@ -38,9 +38,6 @@
 
 #include "internal.h"
 #include "mount.h"
-#ifdef CONFIG_SDCARD_FS
-#include "sdcardfs/sdcardfs.h"
-#endif
 
 /* [Feb-1997 T. Schoebel-Theuer]
  * Fundamental changes in the pathname lookup mechanisms (namei)
