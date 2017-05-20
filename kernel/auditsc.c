62f35f2f2d64374b2387c064685ff74cbd06ec84
gugu0das <gugu0das@gmail.com>
2017-05-20 21:51:13 +0900

    kernel : auditsc : Revert 'Remove audit_proctitle_rtrim()'
    This reverts commit afc1f967efe5a14fa18d18d3a7393a531c32537f.

--- kernel/auditsc.c
+++ kernel/auditsc.c
@@ -1589,6 +1589,18 @@ static void audit_log_name(struct audit_context *context, struct audit_names *n,
 	audit_log_end(ab);
 }
 
+static inline int audit_proctitle_rtrim(char *proctitle, int len)
+{
+	char *end = proctitle + len - 1;
+	while (end > proctitle && !isprint(*end))
+		end--;
+
+	/* catch the case where proctitle is only 1 non-print character */
+	len = end - proctitle + 1;
+	len -= isprint(proctitle[len-1]) == 0;
+	return len;
+}
+
 static void audit_log_proctitle(struct task_struct *tsk,
 			 struct audit_context *context)
 {
