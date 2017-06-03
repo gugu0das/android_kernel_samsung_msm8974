fa44ea0f3b6e09139e8de5c35bfda7581d58048e
gugu0das <gugu0das@gmail.com>
2017-06-03 17:22:33 +0900

    mm : vmscan : Remove SetPageWasActive(page)

--- mm/vmscan.c
+++ mm/vmscan.c
@@ -1301,9 +1301,6 @@ putback_inactive_pages(struct mem_cgroup_zone *mz,
 		add_page_to_lru_list(zone, page, lru);
 
 		file = is_file_lru(lru);
-		if (IS_ENABLED(CONFIG_ZCACHE))
-			if (file)
-				SetPageWasActive(page);
 		if (is_active_lru(lru)) {
 			int numpages = hpage_nr_pages(page);
 			reclaim_stat->recent_rotated[file] += numpages;
@@ -1597,12 +1594,6 @@ static void shrink_active_list(unsigned long nr_to_scan,
 		}
 
 		ClearPageActive(page);	/* we are de-activating */
-		if (IS_ENABLED(CONFIG_ZCACHE))
-			/*
-			 * For zcache to know whether the page is from active
-			 * file list
-			 */
-			SetPageWasActive(page);
 		list_add(&page->lru, &l_inactive);
 	}
 
