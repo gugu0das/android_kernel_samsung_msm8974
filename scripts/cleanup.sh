3b60b7bd4b76e395003944f190449dc40635d1b0
gugu0das <gugu0das@gmail.com>
2017-06-05 15:13:06 +0900

    scripts : Add Build Scripts

---
+++ scripts/cleanup.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+make mrproper
+rm -r -f output/
---
+++ scripts/cm_jactivelteskt.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+export ARCH=arm
+export CROSS_COMPILE=$(pwd)/armeb-linux-gnueabihf-linaro_6.3.1-2017.02/bin/armeb-linux-gnueabihf-
+# export CROSS_COMPILE=$(pwd)/arm-eabi-ubertc_7.0-2016.04/bin/arm-eabi-
+
+mkdir output
+make -C $(pwd) O=output cm_msm8974_sec_defconfig VARIANT_DEFCONFIG=msm8974_sec_jactive_skt_defconfig SELINUX_DEFCONFIG=selinux_defconfig
+make -j64 -C $(pwd) O=output
