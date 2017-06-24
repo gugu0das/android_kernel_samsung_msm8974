8266de3d5c2b88925969cb50bc9192fe744378af
gugu0das <gugu0das@gmail.com>
2017-06-24 23:50:36 +0900

    arm : return_address : Fix return_address Warning

--- arch/arm/kernel/return_address.c
+++ arch/arm/kernel/return_address.c
@@ -55,6 +55,14 @@ void *return_address(unsigned int level)
 	else
 		return NULL;
 }
+
+#else /* if defined(CONFIG_FRAME_POINTER) && !defined(CONFIG_ARM_UNWIND) */
+
+void *return_address(unsigned int level)
+{
+	return NULL;
+}
+
 #endif /* if defined(CONFIG_FRAME_POINTER) && !defined(CONFIG_ARM_UNWIND) / else */
 
 EXPORT_SYMBOL_GPL(return_address);
