81880e4935061024c09a14cdd8488fa163cbc86d
gugu0das <gugu0das@gmail.com>
2017-06-28 17:03:44 +0900

    scripts : Set Linaro 7.1.1 2017.05

--- scripts/cleanup_all.sh
+++ scripts/cleanup_all.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 make mrproper
 rm -r -f output/
-rm -r -f armeb-linux-gnueabihf-linaro_6.3.1-2017.02/
+rm -r -f armeb-linux-gnueabihf-linaro_7.1.1-2017.05/
 rm -r -f arm-eabi-ubertc_7.0-2016.04/
