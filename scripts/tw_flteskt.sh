85195f4ab621715e94f658ec5abcfa1f2a852d26
gugu0das <gugu0das@gmail.com>
2017-06-30 00:50:17 +0900

    scripts : Add flteskt Build Script

---
+++ scripts/tw_flteskt.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+export ARCH=arm
+export CROSS_COMPILE=$(pwd)/armeb-linux-gnueabihf-linaro_7.1.1-2017.05/bin/armeb-linux-gnueabihf-
+# export CROSS_COMPILE=$(pwd)/arm-eabi-ubertc_7.0-2016.04/bin/arm-eabi-
+
+mkdir output
+make -C $(pwd) O=output msm8974_sec_defconfig VARIANT_DEFCONFIG=msm8974_sec_flte_skt_defconfig SELINUX_DEFCONFIG=selinux_defconfig
+make -j64 -C $(pwd) O=output
