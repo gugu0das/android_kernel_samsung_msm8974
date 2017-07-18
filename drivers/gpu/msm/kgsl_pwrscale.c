d87282e56208ead0fb060b64c329ba949cc2ab1d
gugu0das <gugu0das@gmail.com>
2017-07-18 21:41:32 +0900

    kgsl : Revert 'Delay the first devfreq status read'
    This reverts commit 95f92356b3793fa8d16f1a4026b1ab8f80593f95.

--- drivers/gpu/msm/kgsl_pwrscale.c
+++ drivers/gpu/msm/kgsl_pwrscale.c
@@ -67,8 +67,7 @@ void kgsl_pwrscale_wake(struct kgsl_device *device)
 
 	device->pwrscale.time = ktime_to_us(ktime_get());
 
-	device->pwrscale.next_governor_call = jiffies +
-			msecs_to_jiffies(KGSL_GOVERNOR_CALL_INTERVAL);
+	device->pwrscale.next_governor_call = 0;
 
 	/* to call devfreq_resume_device() from a kernel thread */
 	queue_work(device->pwrscale.devfreq_wq,
@@ -110,6 +109,9 @@ void kgsl_pwrscale_update(struct kgsl_device *device)
 
 	if (!device->pwrscale.enabled)
 		return;
+
+	if (device->pwrscale.next_governor_call == 0)
+		device->pwrscale.next_governor_call = jiffies;
 
 	if (time_before(jiffies, device->pwrscale.next_governor_call))
 		return;
@@ -477,8 +479,7 @@ int kgsl_pwrscale_init(struct device *dev, const char *governor)
 	INIT_WORK(&pwrscale->devfreq_resume_ws, do_devfreq_resume);
 	INIT_WORK(&pwrscale->devfreq_notify_ws, do_devfreq_notify);
 
-	pwrscale->next_governor_call = jiffies +
-			msecs_to_jiffies(KGSL_GOVERNOR_CALL_INTERVAL);
+	pwrscale->next_governor_call = 0;
 
 	return 0;
 }
