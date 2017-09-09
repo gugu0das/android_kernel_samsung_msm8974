9a6a8c01fd5cc900917cb429abb0f579ba26b47c
tioguda <guda.flavio@gmail.com>
2017-09-09 14:15:06 -0300

    msm_limiter: Update msm_limiter to 5.3 (credits neobuddy89)

--- drivers/cpufreq/cpufreq.c
+++ drivers/cpufreq/cpufreq.c
@@ -32,6 +32,11 @@
 
 #include <trace/events/power.h>
 
+#ifdef CONFIG_MSM_LIMITER
+#include <linux/msm_thermal.h>
+#include <soc/qcom/limiter.h>
+#endif
+
 /**
  * The "cpufreq driver" - the arch- or hardware-dependent low
  * level driver of CPUFreq support, and its spinlock. This lock
@@ -43,7 +48,7 @@ static DEFINE_PER_CPU(struct cpufreq_policy *, cpufreq_cpu_data);
 /* This one keeps track of the previously set governor of a removed CPU */
 struct cpufreq_cpu_save_data {
 	char gov[CPUFREQ_NAME_LEN];
-	unsigned int max, min;
+	unsigned int max, min, max_freq, min_freq;
 };
 static DEFINE_PER_CPU(struct cpufreq_cpu_save_data, cpufreq_policy_save);
 #endif
@@ -430,15 +435,29 @@ show_one(cpu_load, load_at_max);
 static int __cpufreq_set_policy(struct cpufreq_policy *data,
 				struct cpufreq_policy *policy);
 
+static bool cpufreq_update_allowed(int mpd)
+{
+#ifdef CONFIG_MSM_LIMITER
+	if (mpd == 0 && mpd_enabled == 0)
+		return false;
+#endif
+
+	return true;
+}
+
 /**
  * cpufreq_per_cpu_attr_write() / store_##file_name() - sysfs write access
  */
-#define store_one(file_name, object)			\
+#define store_one(file_name, object)					\
 static ssize_t store_##file_name					\
 (struct cpufreq_policy *policy, const char *buf, size_t count)		\
 {									\
 	unsigned int ret = -EINVAL;					\
 	struct cpufreq_policy new_policy;				\
+	int mpd = strcmp(current->comm, "mpdecision");			\
+									\
+	if (!cpufreq_update_allowed(mpd))				\
+		return ret;						\
 									\
 	ret = cpufreq_get_policy(&new_policy, policy->cpu);		\
 	if (ret)							\
@@ -1964,7 +1983,7 @@ skip:
 EXPORT_SYMBOL(cpufreq_set_freq);
 
 /*
- *	cpufreq_get_max - get max freq of a cpu
+ *	cpufreq_get_max - get policy max freq of a cpu
  *	@cpu: CPU whose max frequency needs to be known
  */
 int cpufreq_get_max(unsigned int cpu)
@@ -1982,7 +2001,7 @@ int cpufreq_get_max(unsigned int cpu)
 EXPORT_SYMBOL(cpufreq_get_max);
 
 /*
- *	cpufreq_get_min - get min freq of a cpu
+ *	cpufreq_get_min - get policy min freq of a cpu
  *	@cpu: CPU whose min frequency needs to be known
  */
 int cpufreq_get_min(unsigned int cpu)
@@ -1998,6 +2017,42 @@ int cpufreq_get_min(unsigned int cpu)
 	return freq;
 }
 EXPORT_SYMBOL(cpufreq_get_min);
+
+/*
+ *	cpuinfo_get_max - get real max freq of a cpu
+ *	@cpu: CPU whose max frequency needs to be known
+ */
+int cpuinfo_get_max(unsigned int cpu)
+{
+	struct cpufreq_policy *policy = cpufreq_cpu_get(cpu);
+	unsigned int freq = per_cpu(cpufreq_policy_save, cpu).max_freq;
+
+	if (policy) {
+		freq = policy->cpuinfo.max_freq;
+		cpufreq_cpu_put(policy);
+	}
+
+	return freq;
+}
+EXPORT_SYMBOL(cpuinfo_get_max);
+
+/*
+ *	cpuinfo_get_min - get real min freq of a cpu
+ *	@cpu: CPU whose min frequency needs to be known
+ */
+int cpuinfo_get_min(unsigned int cpu)
+{
+	struct cpufreq_policy *policy = cpufreq_cpu_get(cpu);
+	unsigned int freq = per_cpu(cpufreq_policy_save, cpu).min_freq;
+
+	if (policy) {
+		freq = policy->cpuinfo.min_freq;
+		cpufreq_cpu_put(policy);
+	}
+
+	return freq;
+}
+EXPORT_SYMBOL(cpuinfo_get_min);
 
 /*
  *	cpufreq_set_gov - set governor for a cpu
--- include/soc/qcom/limiter.h
+++ include/soc/qcom/limiter.h
@@ -1,44 +1,15 @@
-#define MSM_LIMITER			"msm_limiter"
-#define LIMITER_ENABLED			1
-#if defined(CONFIG_ARCH_MSM8916)
-#define DEFAULT_SUSPEND_FREQUENCY	998400
-#else
-#define DEFAULT_SUSPEND_FREQUENCY	1728000
-#endif
-#if defined(CONFIG_ARCH_APQ8084)
-#define DEFAULT_RESUME_FREQUENCY	2649600
-#elif defined(CONFIG_ARCH_MSM8916)
-#define DEFAULT_RESUME_FREQUENCY	1209600
-#else
-#define DEFAULT_RESUME_FREQUENCY	2457600
-#endif
-#if defined(CONFIG_ARCH_MSM8916)
-#define DEFAULT_MIN_FREQUENCY		200000
-#else
-#define DEFAULT_MIN_FREQUENCY		300000
-#endif
+#ifndef __LINUX_MSM_LIMITER_H
+#define __LINUX_MSM_LIMITER_H
 
-static struct cpu_limit {
-	unsigned int limiter_enabled;
-	uint32_t suspend_max_freq;
-	uint32_t resume_max_freq_all;
-	uint32_t resume_max_freq[4];
-	uint32_t suspend_min_freq_all;
-	uint32_t suspend_min_freq[4];
-	struct mutex msm_limiter_mutex[4];
-	char *scaling_governor[4];
-	struct notifier_block notif;
-} limit = {
-	.limiter_enabled = LIMITER_ENABLED,
-	.suspend_max_freq = DEFAULT_SUSPEND_FREQUENCY,
-	.resume_max_freq_all = DEFAULT_RESUME_FREQUENCY,
-	.resume_max_freq[0] = DEFAULT_RESUME_FREQUENCY,
-	.resume_max_freq[1] = DEFAULT_RESUME_FREQUENCY,
-	.resume_max_freq[2] = DEFAULT_RESUME_FREQUENCY,
-	.resume_max_freq[3] = DEFAULT_RESUME_FREQUENCY,
-	.suspend_min_freq_all = DEFAULT_MIN_FREQUENCY,
-	.suspend_min_freq[0] = DEFAULT_MIN_FREQUENCY,
-	.suspend_min_freq[1] = DEFAULT_MIN_FREQUENCY,
-	.suspend_min_freq[2] = DEFAULT_MIN_FREQUENCY,
-	.suspend_min_freq[3] = DEFAULT_MIN_FREQUENCY,
-};
+extern unsigned int mpd_enabled;
+
+int cpufreq_set_gov(char *target_gov, unsigned int cpu);
+char *cpufreq_get_gov(unsigned int cpu);
+int cpufreq_set_freq(unsigned int max_freq, unsigned int min_freq,
+			unsigned int cpu);
+int cpufreq_get_max(unsigned int cpu);
+int cpufreq_get_min(unsigned int cpu);
+int cpuinfo_get_max(unsigned int cpu);
+int cpuinfo_get_min(unsigned int cpu);
+
+#endif /* __LINUX_MSM_LIMITER_H */
