4f98b8edd070e7ca0be7333bedc4670ab5fefea4
gugu0das <gugu0das@gmail.com>
2019-01-19 00:25:50 +0900

    Revert "mm: Backport ZCache from QC kernel 3.18"
    This reverts commit daca96edb5b080412d1a981f6192aa4d38414242.

--- include/linux/page-flags.h
+++ include/linux/page-flags.h
@@ -113,9 +113,6 @@ enum pageflags {
 	PG_scfslower,
 	PG_nocache,
 #endif
-#ifdef CONFIG_ZCACHE
-	PG_was_active,
-#endif
 #ifdef CONFIG_KSM_CHECK_PAGE
 	PG_ksm_scan0,		/* page has been scanned by even KSM cycle */
 #endif
@@ -228,11 +225,6 @@ PAGEFLAG(Reserved, reserved) __CLEARPAGEFLAG(Reserved, reserved)
 PAGEFLAG(SwapBacked, swapbacked) __CLEARPAGEFLAG(SwapBacked, swapbacked)
 
 __PAGEFLAG(SlobFree, slob_free)
-#ifdef CONFIG_ZCACHE
-PAGEFLAG(WasActive, was_active)
-#else
-PAGEFLAG_FALSE(WasActive)
-#endif
 #ifdef CONFIG_KSM_CHECK_PAGE
 CLEARPAGEFLAG(KsmScan0, ksm_scan0) TESTSETFLAG(KsmScan0, ksm_scan0)
 CLEARPAGEFLAG(KsmScan1, ksm_scan1) TESTSETFLAG(KsmScan1, ksm_scan1)
